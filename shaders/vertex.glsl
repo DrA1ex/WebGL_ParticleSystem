9cb2bf057959c7e9890490eb74915f33afcb6cab
Alexander <DrA1ex@users.noreply.github.com>
2022-09-22 00:26:13 +0500

    Fix point size issues on some platforms

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -10,5 +10,6 @@ out vec2 a_velocity;
 void main() {
     vec2 translated_pos = a_position.xy / u_resolution * 2.0 - 1.0;
     gl_Position = vec4(translated_pos * vec2(1, -1.0), 0, 1);
+    gl_PointSize = 1.0;
     a_velocity = vec2(a_position.zw);
 }
